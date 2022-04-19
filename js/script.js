2796461bc49241e542afb45aed0ed44287e32b62
Boris <sslonovborisss@gmail.com>
2022-04-19 14:25:49 +0300

    add new blocks + 1 slider

--- js/script.js
+++ js/script.js
@@ -20,6 +20,21 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+//shop-select
+let select = document.querySelector(".shop-select");
+let block = document.querySelectorAll(".shop-block");
+let lastIndex = 0; // После каждой смены опции, сохраняем сюда индекс предыдущего блока
+
+select.addEventListener("change", function () {
+  block[lastIndex].style.display = "none";
+  // Чтобы сразу делать именно его невидимым при следующей смене
+
+  let index = select.selectedIndex; // Определить индекс выбранной опции
+  block[index].style.display = "block"; // Показать блок с соответствующим индексом
+
+  lastIndex = index; // Обновить сохраненный индекс.
+});
+
 //countdown
 $(document).ready(function () {
   // Grab the current date
@@ -36,19 +51,61 @@ $(document).ready(function () {
     countdown: true,
     showSeconds: true,
   });
+
+  // tabs
+  $(".js-tab-trigger").click(function () {
+    var id = $(this).attr("data-tab"),
+      content = $('.js-tab-content[data-tab="' + id + '"]');
+
+    $(".js-tab-trigger.active").removeClass("active");
+    $(this).addClass("active");
+
+    $(".js-tab-content.active").removeClass("active");
+    content.addClass("active");
+  });
+
+  //scrollUp
+  // при нажатии на кнопку scrollup
+  $(".scrollup").click(function () {
+    // переместиться в верхнюю часть страницы
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      1000
+    );
+  });
+});
+// при прокрутке окна (window)
+$(window).scroll(function () {
+  // если пользователь прокрутил страницу более чем на 200px
+  if ($(this).scrollTop() > 200) {
+    // то сделать кнопку scrollup видимой
+    $(".scrollup").fadeIn();
+  }
+  // иначе скрыть кнопку scrollup
+  else {
+    $(".scrollup").fadeOut();
+  }
 });
 
-//map-select
-let select = document.querySelector(".shop-select");
-let block = document.querySelectorAll(".shop-block");
-let lastIndex = 0; // После каждой смены опции, сохраняем сюда индекс предыдущего блока
-
-select.addEventListener("change", function () {
-  block[lastIndex].style.display = "none";
-  // Чтобы сразу делать именно его невидимым при следующей смене
-
-  let index = select.selectedIndex; // Определить индекс выбранной опции
-  block[index].style.display = "block"; // Показать блок с соответствующим индексом
-
-  lastIndex = index; // Обновить сохраненный индекс.
+//tabs-slider
+const swiper = new Swiper(".swiper-types-doors", {
+  // Optional parameters
+  slidesPerView: 1,
+  loop: true,
+  centeredSlides: true,
+  spaceBetween: 20,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // when window width is <= 499px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 0,
+    },
+  },
 });
