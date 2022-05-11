b2c6cf5032caeee2e921ba1a2a26274a846ba902
Boris <sslonovborisss@gmail.com>
2022-05-11 13:04:03 +0300

    add fix

--- js/script.js
+++ js/script.js
@@ -44,6 +44,48 @@ for (let anchor of anchors) {
   });
 }
 
+// Отправка данных на сервер
+function send(event, php) {
+  console.log("Отправка запроса");
+  event.preventDefault ? event.preventDefault() : (event.returnValue = false);
+  var req = new XMLHttpRequest();
+  req.open("POST", php, true);
+  req.onload = function () {
+    if (req.status >= 200 && req.status < 400) {
+      json = JSON.parse(this.response); //  internet explorer 11
+      console.log(json);
+
+      // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
+      if (json.result == "success") {
+        // Если сообщение отправлено
+
+        $.fancybox.open(
+          '<div class="message"><h3 class="message__h3">Спасибо за заявку!</h3><p class="message__p">Мы свяжемся с вами в ближайшее время</p></div>'
+        );
+
+        function closePopup() {
+          $.fancybox.close();
+          $.fancybox.close();
+        }
+
+        setTimeout(closePopup, 3000);
+      } else {
+        // Если произошла ошибка
+        alert("Ошибка. Сообщение не отправлено");
+      }
+      // Если не удалось связаться с php файлом
+    } else {
+      alert("Ошибка сервера. Номер: " + req.status);
+    }
+  };
+
+  // Если не удалось отправить запрос. Стоит блок на хостинге
+  req.onerror = function () {
+    alert("Ошибка отправки запроса");
+  };
+  req.send(new FormData(event.target));
+}
+
 //shop-select
 let citySelect = document.querySelector(".city-select");
 let cityBlock = document.querySelectorAll(".city-block");
@@ -78,13 +120,15 @@ citySelect.addEventListener("change", function () {
   lastIndex = index; // Обновить сохраненный индекс.
 });
 
+
+
 $(document).ready(function () {
   //countdown
   // Grab the current date
   var currentDate = new Date();
   // Set some date in the future. ***change to desired date***
   //var futureDate = new Date(2014, 11, 23, 6, 0, 0);
-  var futureDate = new Date(2022, 4); //fixed as per comments
+  var futureDate = new Date(2022, 5); //fixed as per comments
   // Calculate the difference in seconds between the future and current date
   var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
 
@@ -162,8 +206,11 @@ $(document).ready(function () {
     var offset = $element.offset().top;
 
     if (scroll > offset) {
-      $(".scrollup").css({ position: "absolute", top: "-80px", bottom: "auto" });
-
+      $(".scrollup").css({
+        position: "absolute",
+        top: "-80px",
+        bottom: "auto",
+      });
     } else {
       $(".scrollup").css({ position: "fixed", top: "auto", bottom: "30px" });
     }
---
+++ mailer/send.php
@@ -0,0 +1,65 @@
+<?php
+// Файлы phpmailer
+require 'PHPMailer.php';
+require 'SMTP.php';
+require 'Exception.php';
+
+
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+
+
+
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.yandex.ru'; // SMTP сервера вашей почты
+    $mail->Username   = 'sslonovboriss@yandex.ru'; // Логин на почте
+    $mail->Password   = 'hiadcgwmbegwskev'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->From = "info@estet-invisible.ru";
+    $mail->FromName = "Estet сайт";
+
+   // Получатель письма
+   $mail->addAddress('sslonovborisss@gmail.com'); 
+
+    
+
+// Формирование самого письма
+$title = "Заявка с сайта estet-invisible";
+$body = "
+<h2>Заявка на звонок</h2>
+<b>Имя:</b> $name <br>
+<b>Телефон:</b> $phone
+";
+
+
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
+
+?>
